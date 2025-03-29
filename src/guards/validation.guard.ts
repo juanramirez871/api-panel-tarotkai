6dffde15ecd538710f6f6738cc55beeba873e05e
Juan Ramirez <juandiegoramirez071@gmail.com>
2025-03-29 16:29:34 -0500

    validation refactor

--- src/guards/validation.guard.ts
+++ src/guards/validation.guard.ts
@@ -1,20 +1,47 @@
 import { CanActivate, ExecutionContext, Injectable, BadRequestException } from '@nestjs/common';
-import { Observable } from 'rxjs';
-import { z, ZodSchema } from 'zod';
+import { ZodSchema } from 'zod';
 
 @Injectable()
 export class ValidationGuard implements CanActivate {
-  constructor(private schema: ZodSchema) {}
+  constructor(private schema: ZodSchema<any>) {}
 
-  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> {
     const request = context.switchToHttp().getRequest();
-    const dataToValidate = request.body;
+    const dataToValidate = {
+      ...request.body,
+      ...request.query,
+      ...request.params,
+    };
 
-    try {
-      this.schema.parse(dataToValidate);
-      return true;
-    } catch (error) {
-      throw new BadRequestException(error.errors);
+    const result = this.schema.safeParse(dataToValidate);
+
+    if (!result.success) {
+      if (request.method === 'POST') {
+        const errorsObject: Record<string, string[]> = {};
+
+        result.error.errors.forEach(error => {
+          const fieldName = error.path[0] as string;
+          const errorMessage = error.message;
+
+          if (!errorsObject[fieldName]) {
+            errorsObject[fieldName] = [];
+          }
+
+          errorsObject[fieldName].push(`${errorMessage}`);
+        });
+
+        throw new BadRequestException({ errors: errorsObject });
+      } else {
+        const firstError = result.error.errors[0];
+        const fieldName = firstError.path[0] as string;
+        const errorMessage = firstError.message;
+
+        throw new BadRequestException({
+          error: `El ${fieldName ? ' ' : ''}${errorMessage}`,
+        });
+      }
     }
+
+    return true;
   }
 }
--- src/modules/auth/auth.schema.ts
+++ src/modules/auth/auth.schema.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
 export const loginSchema = z.object({
-  email: z.string().min(1, 'Email is obligatorio'),
+  email: z.string().nonempty('El correo es obligatorio').min(1, 'Email is obligatorio'),
   password: z.string().min(6, 'La contraseña debe tener al menos 6 caracteres'),
 });
