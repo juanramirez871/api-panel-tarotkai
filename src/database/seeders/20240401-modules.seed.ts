05426a293ebbaa7d455af504d71232e33785d159
Juan Ramirez <juandiegoramirez071@gmail.com>
2025-04-03 22:53:16 -0500

    fixed seed

--- src/database/seeders/20240401-modules.seed.ts
+++ src/database/seeders/20240401-modules.seed.ts
@@ -7,7 +7,7 @@ const ModulesWithSubmodules = [
     { name: 'metas', icon: 'Star', route: "goals" },
     { name: 'reportes', icon: 'Document', route: "reports" },
     { name: 'metricas', icon: 'Histogram', route: "metrics" },
-    { name: 'configuracion', icon: 'setting', route: "configuration" },
+    { name: 'configuracion', icon: 'Setting', route: "configuration" },
     { name: 'configuracion_permisos', icon: 'Lock', route: "privileges" },
     { name: 'configuracion_usuarios', icon: 'User', route: "users" },
     { name: 'configuracion_tipo_de_llamada', icon: 'Iphone', route: "type-calls" },
